e1d9239307df788cb2655c356dfd83a1a04b11c9
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-07-24 16:15:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,9 +16,9 @@ class App < Sinatra::Base
   get '/favorite-song' do
     "My favorite song is 'Imagine', by John Lennon"
   end
-  
+
 end
 
 
-#The name route should display "My name is __" in the browser, the hometown route should display 
+#The name route should display "My name is __" in the browser, the hometown route should display
 #"My hometown is __", and the favorite-song route should display "My favorite song is __".
